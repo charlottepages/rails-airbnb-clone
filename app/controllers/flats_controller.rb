a5b917dd9521517040c17bbd83363ea1055beb47
Charlotte Pages <pgs.charlotte@gmail.com>
2018-08-13 23:09:55 +0200

    airba, better than airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,19 @@
 require 'open-uri'
 
 class FlatsController < ApplicationController
-  def list
-    @flats = JSON.parse(open('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json').read)
+  def index
+    @flats = parse_flats
   end
 
   def show
+    @flat = parse_flats.find do |flat|
+      flat['id'].to_i == params[:id].to_i
+    end
+  end
+
+  private
+
+  def parse_flats
+    JSON.parse(open('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json').read)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'list', to: 'flats#list', as: :list
-  get 'show', to: 'flats#show', as: :show
+  get 'flats', to: 'flats#index', as: :flats
+  get 'flats/:id', to: 'flats#show', as: :flat
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
